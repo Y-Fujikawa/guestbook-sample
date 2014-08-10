bceaceec732d564d369e805d80423ba98defbbe4
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2014-08-10 16:08:08 +0900

    サンプルデータ投入のためにseeds.rbを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do |no|
+  Greeting.create(name: "テスト#{no}", comment: "コメント#{no}")
+end
