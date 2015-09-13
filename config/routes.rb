ed50d587551fb332876629addbddf2fda72edfaa
Y-Fujikawa <fujikawa0@gmail.com>
2015-09-14 00:14:36 +0900

    Webページを言語切替できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :greetings, only: [:index, :create]
-  root 'greetings#index'
+  scope "(:locale)" do
+    resources :greetings, only: [:index, :create]
+    root 'greetings#index'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
