77ac9ec3271e08306a1694b9bfe9fbb3787d0cb6
Y-Fujikawa <fujikawa0@gmail.com>
2015-09-13 18:39:19 +0900

    Revert "SECRET_KEY_BASEの設定方法を変更"
    This reverts commit 90980842a64a9bfa4211b09ec570fb350d4c0ed1.

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :scm, :git
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml .env}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
@@ -54,6 +54,7 @@ namespace :deploy do
         execute "mkdir -p #{shared_path}/config"
       end
       upload!('config/database.yml', "#{shared_path}/config/database.yml")
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
     end
   end
 
