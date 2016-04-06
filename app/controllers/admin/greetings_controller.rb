ce32df81fc857dd48b1d65ca5de1abba874a703b
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2016-04-06 09:44:14 +0900

    一覧ページにデータを表示するようにした

--- app/controllers/admin/greetings_controller.rb
+++ app/controllers/admin/greetings_controller.rb
@@ -1,4 +1,12 @@
 class Admin::GreetingsController < Admin::AdminController
+  before_action :set_greetings, only: [:index]
+
   def index
+  end
+
+  private
+
+  def set_greetings
+    @greetings = Greeting.all.order("created_at").page(params[:page]).per(5)
   end
 end
