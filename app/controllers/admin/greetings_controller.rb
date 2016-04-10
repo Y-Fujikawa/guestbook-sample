089448c5581a9771f1600c36819b591ecff1a67d
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2016-04-10 15:25:38 +0900

    管理画面に編集画面を作成

--- app/controllers/admin/greetings_controller.rb
+++ app/controllers/admin/greetings_controller.rb
@@ -1,20 +1,36 @@
 class Admin::GreetingsController < Admin::AdminController
   before_action :set_greetings, only: [:index]
+  before_action :get_greeting, only: [:show, :edit, :update]
 
   def index
   end
 
   def show
-    @greeting = Greeting.find(params[:id])
+  end
+
+  def edit
   end
 
   def update
-    @greeting = Greeting.find(params[:id])
+    if @greeting.update_attributes(greeting_params)
+      flash[:success] = "Profile updated"
+      redirect_to admin_greetings_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def set_greetings
     @greetings = Greeting.all.order("created_at").page(params[:page]).per(5)
+  end
+
+  def get_greeting
+    @greeting = Greeting.find(params[:id])
+  end
+
+  def greeting_params
+    params.require(:greeting).permit(:name, :comment)
   end
 end
