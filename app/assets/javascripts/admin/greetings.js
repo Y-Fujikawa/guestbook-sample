0e315563530aded13363a4ca4b075a4205aef72b
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2016-04-15 09:28:08 +0900

    公開非公開ボタンを追加

--- app/assets/javascripts/admin/greetings.js
+++ app/assets/javascripts/admin/greetings.js
@@ -1,6 +1,19 @@
 $(function() {
-    $("#change_enable").on("ajax:success", function(event, xhr, settings) {
-        alert("hoge");
-        console.log("ajax:success!")
+    $(".change_enable").on("click", function() {
+        var btn = $(this);
+        btn.prop('disabled', true);
+        $.ajax({
+            url: "/ja/admin/greetings/" + $(this).next("input").val() + "/enable",
+        }).done(function(data) {
+            if (btn.text() == "非公開") {
+                btn.text("公開");
+            } else {
+                btn.text("非公開");
+            }
+        }).fail(function(data) {
+            alert("error!");
+        }).always(function(data) {
+            btn.prop('disabled', false);
+        });
     });
 });
