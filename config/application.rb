40e015f2eaae224a334620a199a8b138c2e77c43
Y-Fujikawa <fujikawa0@gmail.com>
2016-04-18 00:16:08 +0900

    stagingでfontawesomeが表示されない不具合を修正

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,15 @@ Bundler.require(*Rails.groups)
 
 module GuestbookSample
   class Application < Rails::Application
+				# Bower asset paths
+				root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+						config.sass.load_paths << bower_path
+						config.assets.paths << bower_path
+				end
+				# Precompile Bootstrap fonts
+				config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+				config.assets.precompile << %r(fontawesome/fonts/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
