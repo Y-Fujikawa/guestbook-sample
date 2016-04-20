1781a96a71da6afcecd5d1e1c73174e5d4473239
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2016-04-20 20:21:04 +0900

    fontawesomeに置き換わっていなかったところを修正
    同時にエラーメッセージも修正

--- app/controllers/admin/devise/passwords_controller.rb
+++ app/controllers/admin/devise/passwords_controller.rb
@@ -1,3 +1,8 @@
 class Admin::Devise::PasswordsController < Devise::PasswordsController
   layout "admin/layouts/login"
+
+  # Admin画面のログイン -> Adminホーム画面へリダイレクト
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
 end
