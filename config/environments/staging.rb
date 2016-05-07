f6a674ca3752a9bbd0caf76ccc55f5640863ebc7
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2016-05-07 16:15:33 +0900

    ENVの誤字を修正

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
     port: ENV['SMTP_PORT'],
-    user_name: ENV['SMTP_PASSWORD'],
+    user_name: ENV['SMTP_USER_NAME'],
     password: ENV['SMTP_PASSWORD'],
     domain: ENV['DOMAIN'],
     authentication: :plain,
