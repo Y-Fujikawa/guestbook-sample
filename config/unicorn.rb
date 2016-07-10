47f343d8192a4ae6cfd2f7e4481bc4879e6b6b38
Y-Fujikawa <fujikawa0@gmail.com>
2016-07-10 23:12:29 +0900

    unicronのold pid削除をサンプルどおりにした
    @see https://github.com/defunkt/unicorn/blob/master/examples/unicorn.conf.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -26,13 +26,14 @@ stderr_path "#{ROOT}/log/unicorn-stderr.log"
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 
-  old_pid = "#{ server.config[:pid] }.oldbin"
-  unless old_pid == server.pid
-    begin
-      Process.kill :QUIT, File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
+	old_pid = "#{server.config[:pid]}.oldbin"
+	if old_pid != server.pid
+	  begin
+	    sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+	    Process.kill(sig, File.read(old_pid).to_i)
+	  rescue Errno::ENOENT, Errno::ESRCH
+	  end
+	end
 end
 
 after_fork do |server, worker|
